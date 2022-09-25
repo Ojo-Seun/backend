74df0cb1fd8d6ed910c4f46c324b12d9d972cde2
OJO SEUN <ojoseun399@gmail.com>
2022-09-25 16:05:27 +0100

    Fly Backend Initial Setup

---
+++ products.ts
@@ -0,0 +1,32 @@
+// export const products = [
+//     {
+//     id: 0,
+//     name: 'DJI MAVIC 2 PRO',
+//     slug: 'dji-mavic-2-pro',
+//     price: 200,
+//     brand: 'DJI',
+//     modelName: 'k3.2018082121',
+//     VCR: '4KHD',
+//     countInStock: 4,
+//     rating: 2.5,
+//     sellerName: 'op',
+//     image: "image/dji",
+//     category:"quadcopter"
+//     },
+//     {
+//     id: 1,
+//     name: 'DJI MAVIC 2 PRO',
+//     slug: 'dji-mavic-2-pro',
+//     price: 200,
+//     brand: 'DJI',
+//     modelName: 'k3.2018082121',
+//     VCR: '4KHD',
+//     countInStock: 4,
+//     rating: 2.5,
+//     sellerName: 'po',
+//       image: "image/dji",
+//     category:"quadcopter"
+//   }
+// ]
+
+
